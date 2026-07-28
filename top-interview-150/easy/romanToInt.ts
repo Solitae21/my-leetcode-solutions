32a0c1fe5b0ba2ea18e93c55036870b6950965f5
HansAmponin <amponinhans@gmail.com>
2026-07-28 15:58:01 +0800

    refactor: improve formatting and readability of romanToInt function

--- top-interview-150/easy/romanToInt.ts
+++ top-interview-150/easy/romanToInt.ts
@@ -1,25 +1,24 @@
 function romanToInt(s: string): number {
-    const roman: Record<string, number> = {
-        I: 1,
-        V: 5,
-        X: 10,
-        L: 50,
-        C: 100,
-        D: 500,
-        M: 1000,
-    };
+  const roman: Record<string, number> = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
 
-    let numeral = 0;
+  let numeral = 0;
 
-    for (let i = 0; i < s.length; i++) {
-        if (roman[s[i]] < roman[s[i + 1]] && s[i + 1]) {
-            numeral += roman[s[i + 1]] - roman[s[i]]
-            i++;
-        } 
-        else {
-            numeral += roman[s[i]]
-        }
+  for (let i = 0; i < s.length; i++) {
+    if (s[i + 1] && roman[s[i]] < roman[s[i + 1]]) {
+      numeral += roman[s[i + 1]] - roman[s[i]];
+      i++;
+    } else {
+      numeral += roman[s[i]];
     }
+  }
 
-    return numeral;
-};
+  return numeral;
+}
